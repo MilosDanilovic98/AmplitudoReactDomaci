41db171548d4ba120079141d2e66b3737934369d
Milos Danilovic <danilovicmilos25@gmail.com>
2022-03-11 12:01:31 +0100

    adding images and some changes to the app.js and app.css(i made the last couple of commits from a wrong directory so it didnt commit the images and app.js adn css changes)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import IdCard from "./components/idCard/IdCard";
+import Image from "./components/imageComponent/Image";
+import Book from "./components/book/Book";
+import Movie from "./components/movieComponent/Movie";
+import List from "./components/list/List";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="listTitle">ID CARDS</h1>
+      <List>
+        <IdCard
+          firstName="Milos"
+          lastName="Danilovic"
+          city="Pljevlja"
+          dateOfBirth="25/11/1998"
+          image={<Image src="profile-pic.png" />}
+        />
+        <IdCard
+          firstName="Milos"
+          lastName="Danilovic"
+          city="Pljevlja"
+          dateOfBirth="25/11/1998"
+          image={<Image src="maleAvatar.jpg" />}
+        />
+        <IdCard
+          firstName="Milos"
+          lastName="Danilovic"
+          city="Pljevlja"
+          dateOfBirth="25/11/1998"
+          image={<Image src="maleAvatar.jpg" />}
+        />
+      </List>
+      <h1 className="listTitle">BOOKS </h1>
+      <List>
+        <Book
+          title="The Martian"
+          year="2011"
+          author="Andy Weir"
+          quote="Mark Watney: I am definitely going to die up here ... if I have to listen to any more god-awful disco music."
+          image={<Image src="theMartian.jpg" />}
+        />
+        <Book
+          title="Robinson Crusoe"
+          year="1719"
+          author="Daniel Defoe"
+          quote="“Thus fear of danger is ten thousand times more terrifying than danger itself.”"
+          image={<Image src="RobinsonCrusoe.webp" />}
+        />
+        <Book
+          title="Misery"
+          year="1987"
+          author="Stephen King"
+          quote="“There may be fairies, there may be elves, but God helps those who help themselves.”"
+          image={<Image src="misery.jpg" />}
+        />
+      </List>
+      <h1 className="listTitle">MOVIES</h1>
+      <List>
+        <Movie
+          title="The Departed"
+          year="2006"
+          director="Martin Scorsese"
+          actors="Leonardo DiCaprio,Matt Damon,Mark Wahlberg"
+          genre="Thriller"
+          image={<Image src="theDeparted.jpg" />}
+        />
+        <Movie
+          title="In Bruges"
+          year="2008"
+          director="Martin McDonagh"
+          actors="Collin Farrell,Brendan Gleeson,Ralph Fines"
+          genre="Dark Comedy"
+          image={<Image src="inBruge.jpg" />}
+        />
+        <Movie
+          title="The Social Network"
+          year="2010"
+          director="David Fincher"
+          actors="Jesse Eisenberg,Andrew Garfield,Justin Timberlake"
+          genre="Drama"
+          image={<Image src="theSocialNetwork.jpg" />}
+        />
+      </List>
     </div>
   );
 }
